943ea8988c7231903efb1a57390c313564f9a7da
RastW <ackermanx@163.com>
2021-03-10 00:07:52 +0800

    my first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # go-epccpayment
+This project well refactor the payment farmework of bancstone
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println("go-epccpayment-start")
+}
