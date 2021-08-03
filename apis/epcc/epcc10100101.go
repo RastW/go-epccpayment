c056bba889e1693f717771d50c9df70d7f6d069c
RastW <ackermanx@163.com>
2021-08-03 14:12:35 +0800

    模版

--- apis/epcc/epcc10100101.go
+++ apis/epcc/epcc10100101.go
@@ -1,4 +1,38 @@
 package epcc
 
 type epcc10100101 struct {
+	msgHander msgHeader `xml:"MsgHeader"`
+	msgBody   msgBody   `xml:"MsgBody"`
+}
+
+type msgHeader struct {
+	sndDt     string `xml:"SndDt"`
+	msgTp     string `xml:"MsgTp"`
+	issrId    string `xml:"IssrId"`
+	drctn     string `xml:"Drctn"`
+	signSN    string `xml:"SignSN"`
+	ncrptnSN  string `xml:"NcrptnSN"`
+	dgtlEnvlp string `xml:"DgtlEnvlp"`
+}
+
+type msgBody struct {
+	sigint sigInf `xml:"SigInf"`
+}
+
+type sigInf struct {
+	sgnAcctIssrId string `xml:"SgnAcctIssrId"`
+	sgnAcctTp     string `xml:"SgnAcctTp"`
+	sgnAcctId     string `xml:"SgnAcctId"`
+	sgnAcctNm     string `xml:"SgnAcctNm"`
+	iDTp          string `xml:"IDTp"`
+	iDNo          string `xml:"IDNo"`
+	mobNo         string `xml:"MobNo"`
+}
+
+type trxInf struct {
+	trxCtgy  string `xml:"TrxCtgy"`
+	trxId    string `xml:"Trxid"`
+	trxDtTm  string `xml:"TrxDtTm"`
+	authMsg  string `xml:"AuthMsg"`
+	clbckUrl string `xml:"ClbckUrl"`
 }
